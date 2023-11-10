a0503f0f6a6f5959202e1f783d96a198473be562
Sam Page <samnpage96@gmail.com>
2023-11-10 14:26:16 -0500

    update annotations

--- RealEstate.Data/Entities/BuyersEntity.cs
+++ RealEstate.Data/Entities/BuyersEntity.cs
@@ -6,18 +6,19 @@ public class BuyersEntity
     [Key]
     public int Id { get; set; }
 
-    [Required]
+    [Required, MinLength(4), MaxLength(100)]
     public string FirstName { get; set; } = string.Empty;
 
-    [Required]
+    [Required, MinLength(4), MaxLength(100)]
     public string LastName { get; set; } = string.Empty;
 
     [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
 
-    [Phone]
+    [Phone, MaxLength(14)]
     public int Phone { get; set; }
 
+    [MaxLength(100)]
     public int PrefSqFt { get; set; }
 
     public DateTime DateCreated { get; set; }
