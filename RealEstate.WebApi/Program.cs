8620ce874c886c6c1e767046c6a37422fd88a006
Sam Page <samnpage96@gmail.com>
2023-11-10 15:08:11 -0500

    password config added

--- RealEstate.WebApi/Program.cs
+++ RealEstate.WebApi/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RealEstate.Data;
+using RealEstate.Data.Entities;
 using RealEstate.Services.Buyers;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,21 @@ builder.Services.AddScoped<IBuyersService, BuyersService>();
 // Add connection string and DbContext setup
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<AgentsEntity>(options =>
+{
+    // Password configuration
+    options.Password.RequiredLength = 4;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+
+
+})
+
+    .AddRoles<IdentityRole<int>>() // Enable Roles
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
