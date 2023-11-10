baccc3ba5bd4da71a85f08b23e2f5e7224d747be
Sam Page <samnpage96@gmail.com>
2023-11-10 16:03:07 -0500

    RegisterAgentsAsync and GetAgentByIdAsync methods added

--- RealEstate.Services/Agents/IAgentsService.cs
+++ RealEstate.Services/Agents/IAgentsService.cs
@@ -1,5 +1,9 @@
+using ElevenNote.Models.Agents;
+using RealEstate.Models.Agents;
+
 namespace RealEstate.Services.Agents;
 public interface IAgentsService
 {
-    
+    Task<bool> RegisterAgentsAsync(AgentsRegister model);
+    Task<AgentsDetail?> GetAgentByIdAsync(int agentId);
 }
