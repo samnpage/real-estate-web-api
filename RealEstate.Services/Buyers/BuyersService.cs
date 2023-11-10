f6e9cbea52d557bce2d215436972a41f210bcfdb
Sam Page <samnpage96@gmail.com>
2023-11-10 15:37:22 -0500

    Agent Id check logic implemented

--- RealEstate.Services/Buyers/BuyersService.cs
+++ RealEstate.Services/Buyers/BuyersService.cs
@@ -7,11 +7,20 @@ using RealEstate.Models.Buyers;
 namespace RealEstate.Services.Buyers;
 public class BuyersService : IBuyersService
 {
-    private readonly ApplicationDbContext _context;
+    private readonly ApplicationDbContext _dbContext;
+    private readonly int _agentId;
 
-    public BuyersService(ApplicationDbContext context)
+    public BuyersService(UserManager<AgentsEntity> userManager, 
+                        SignInManager<AgentsEntity> signInManager, 
+                        ApplicationDbContext dbContext)
     {
-        _context = context;
+        var currentAgent = signInManager.Context.User;
+        var agentIdClaim = userManager.GetUserId(currentAgent);
+        var hasValidId = int.TryParse(agentIdClaim, out _agentId);
+
+        if (hasValidId == false)
+            throw new Exception("Attempted to build BuyersService without Id claim.");
+        _dbContext = dbContext;
     }
 
     // Creates new buyer contact
