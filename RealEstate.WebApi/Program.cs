f6dc02245a25e8b481c6bb2683431e027cd4fe98
Sam Page <samnpage96@gmail.com>
2023-11-10 14:14:13 -0500

    Buyers services added

--- RealEstate.WebApi/Program.cs
+++ RealEstate.WebApi/Program.cs
@@ -1,10 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using RealEstate.Data;
+using RealEstate.Services.Buyers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IBuyersService, BuyersService>();
 // Add connection string and DbContext setup
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
