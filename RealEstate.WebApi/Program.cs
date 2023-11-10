811578ff5e24ffe33f50de0405a01040df5ecc9c
Sam Page <samnpage96@gmail.com>
2023-11-10 15:25:39 -0500

    space

--- RealEstate.WebApi/Program.cs
+++ RealEstate.WebApi/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddScoped<IBuyersService, BuyersService>();
 builder.Services.AddScoped<IAgentsService, AgentsService>();
+
 // Add connection string and DbContext setup
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
