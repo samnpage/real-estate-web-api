801e59cbeb0ece88fe2b4b4c63b04256ddb82d3c
Sam Page <samnpage96@gmail.com>
2023-11-10 15:00:13 -0500

    IdentityDbContext added. OnModelCreating method added

--- RealEstate.Data/ApplicationDbContext.cs
+++ RealEstate.Data/ApplicationDbContext.cs
@@ -1,11 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RealEstate.Data.Entities;
 
 namespace RealEstate.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<AgentsEntity, IdentityRole<int>, int>
 {   
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
 
+
     public DbSet<BuyersEntity> Buyers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<AgentsEntity>().ToTable("Agents");
+        builder.Entity<BuyersEntity>().ToTable("Buyers");
+    }
 }
