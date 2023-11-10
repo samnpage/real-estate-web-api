1c8aef7c720f680249a543fee515a310edc0aa9d
Sam Page <samnpage96@gmail.com>
2023-11-10 14:36:29 -0500

    initial commit

--- RealEstate.Services/Buyers/BuyersService.cs
+++ RealEstate.Services/Buyers/BuyersService.cs
@@ -1,5 +1,29 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Data;
+using RealEstate.Data.Entities;
+using RealEstate.Models.Buyers;
+
 namespace RealEstate.Services.Buyers;
 public class BuyersService : IBuyersService
 {
+    private readonly ApplicationDbContext _context;
 
+    public BuyersService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // Creates new buyer contact
+    public async Task<bool> CreateBuyerContactAsync(CreateBuyers model)
+    {
+        BuyersEntity entity = new()
+        {
+            FirstName = model.FirstName,
+            LastName = model.LastName,
+            Email = model.Email,
+            Phone = model.Phone,
+            PrefSqFt = model.PrefSqFt
+        };
+    }
 }
