d840255a7dcd6163289e98dc1ae12c1a567c6f18
Sam Page <samnpage96@gmail.com>
2023-11-10 16:19:42 -0500

    CRUD comments created

--- RealEstate.Services/Agents/IAgentsService.cs
+++ RealEstate.Services/Agents/IAgentsService.cs
@@ -4,6 +4,13 @@ using RealEstate.Models.Agents;
 namespace RealEstate.Services.Agents;
 public interface IAgentsService
 {
+    // CREATE
     Task<bool> RegisterAgentsAsync(AgentsRegister model);
+
+    // READ
     Task<AgentsDetail?> GetAgentByIdAsync(int agentId);
+
+    // UPDATE
+    
+    // DELETE
 }
