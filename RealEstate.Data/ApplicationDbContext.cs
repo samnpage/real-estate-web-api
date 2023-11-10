323bf809ca60770afe2d287f4b5c0663c4637c85
Sam Page <samnpage96@gmail.com>
2023-11-10 13:12:17 -0500

    Buyers entity added

--- RealEstate.Data/ApplicationDbContext.cs
+++ RealEstate.Data/ApplicationDbContext.cs
@@ -1,8 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using RealEstate.Data.Entities;
 
 namespace RealEstate.Data;
 
 public class ApplicationDbContext : DbContext
 {   
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+    public DbSet<BuyersEntity> Buyers { get; set; }
 }
