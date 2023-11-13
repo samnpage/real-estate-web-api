67a37dc65a9ccb906a8cb55509a64399dbce6076
Deionta Smith <deiontasmith309@gmail.com>
2023-11-12 23:55:25 -0500

    listings

---
+++ RealEstate.Data/Entities/Listings.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RealEstate.Data.Entities
+{
+    public class Listings
+    {
+        [Key]
+        [Required]
+        public int Id {get; set;}
+
+        public string? Address1 {get; set;}
+
+        public string? Address2{get;set;}
+
+        public string? City {get; set;}
+
+        public string? State {get; set;}
+
+        public int ZipCode {get; set;}
+
+        public int SquareFootage {get; set;}
+
+        public decimal Price {get; set;}
+        
+        [MaxLength(300)]
+        public string? FeedBack {get; set;}
+        [ForeignKey]
+        public string? HomeStyle{get; set;}
+    }
+}
