69f4fb0eebc1d95c26c4cd69d082b38f929ab9ca
Sam Page <samnpage96@gmail.com>
2023-11-14 16:35:14 -0500

    Post Method added

--- RealEstate.WebApi/Controllers/BuyersController.cs
+++ RealEstate.WebApi/Controllers/BuyersController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using RealEstate.Data.Entities;
+using RealEstate.Models.Buyers;
+using RealEstate.Models.Responses;
 using RealEstate.Services.Buyers;
 
 namespace RealEstate.WebApi.Controllers;
@@ -11,5 +14,30 @@ public class BuyersController : ControllerBase
     public BuyersController(IBuyersService buyersService)
     {
         _buyersService = buyersService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateBuyer([FromForm] CreateBuyers request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var response = await _buyersService.CreateBuyerContactAsync(request);
+        if (response is not null)
+            return Ok(response);
+
+        return BadRequest(new TextResponse("Could not create new buyer"));
+    }
+
+    [HttpGet("{buyerId:int}")]
+    public async Task<IActionResult> GetBuyerById([FromRoute] int buyerId)
+    {
+        BuyersEntity? entity = await _buyersService.GetBuyerByIdAsync(buyerId);
+
+        return entity is not null
+                ? Ok(entity)
+                : NotFound();
     }
 }
