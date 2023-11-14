368470312644662086ea8ecad9463583905e5efc
Deionta Smith <deiontasmith309@gmail.com>
2023-11-14 14:22:39 -0500

    Added Services and ApplicationDbset property

---
+++ RealEstate.Services/Listings/IListingsService.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RealEstate.Data.Entities;
+
+namespace RealEstate.Services
+{
+    public interface IListingsService
+    {
+        Task<IEnumerable<Listings>> GetAllListingsAsync();
+        Task<Listings> GetListingByIdAsync(int id);
+        Task CreateListingAsync(Listings listing);
+        Task UpdateListingAsync(int id, Listings updatedListing);
+        Task DeleteListingAsync(int id);
+    }
+}
