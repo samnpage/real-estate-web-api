0a6af2ebf23cda0b8d54a2a54224087a38cc73e6
Sam Page <samnpage96@gmail.com>
2023-11-14 16:33:11 -0500

    Phone changed to string

--- RealEstate.Models/Buyers/ListBuyers.cs
+++ RealEstate.Models/Buyers/ListBuyers.cs
@@ -5,7 +5,7 @@ public class ListBuyers
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public int Phone { get; set; }
+    public string Phone { get; set; } = string.Empty;
     public int PrefSqFt { get; set; }
     public DateTime DateCreated { get; set; }
 }
