90b76714d03bb66d4859ad2be5d8c14d00dae10a
Sam Page <samnpage96@gmail.com>
2023-11-14 20:13:38 -0500

    GetAll, Update, and Delete agent methods added

--- RealEstate.Services/Agents/IAgentsService.cs
+++ RealEstate.Services/Agents/IAgentsService.cs
@@ -1,5 +1,7 @@
 using ElevenNote.Models.Agents;
+using RealEstate.Data.Entities;
 using RealEstate.Models.Agents;
+using RealEstate.Models.Responses;
 
 namespace RealEstate.Services.Agents;
 public interface IAgentsService
@@ -8,9 +10,12 @@ public interface IAgentsService
     Task<bool> RegisterAgentsAsync(AgentsRegister model);
 
     // READ
+    Task<List<AgentsEntity>> GetAllAgentsAsync();
     Task<AgentsDetail?> GetAgentByIdAsync(int agentId);
 
     // UPDATE
-    
+    Task<AgentsEntity?> UpdateAgentByIdAsync(int id, UpdateAgent updatedAgent);
+
     // DELETE
+    Task<TextResponse> DeleteAgentByIdAsync(int id);
 }
