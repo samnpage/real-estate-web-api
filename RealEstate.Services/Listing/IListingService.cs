a3348eb25b07dc8fe7dc0d759b4a33a9f9957caa
Sam Page <samnpage96@gmail.com>
2023-11-15 00:21:35 -0500

    initial commit

---
+++ RealEstate.Services/Listing/IListingService.cs
@@ -0,0 +1,13 @@
+using RealEstate.Data.Entities;
+
+namespace RealEstate.Services
+{
+    public interface IListingService
+    {
+        Task<IEnumerable<ListingEntity>> GetAllListingsAsync();
+        Task<ListingEntity> GetListingByIdAsync(int id);
+        Task CreateListingAsync(ListingEntity listing);
+        Task UpdateListingAsync(int id, ListingEntity updatedListing);
+        Task DeleteListingAsync(int id);
+    }
+}
