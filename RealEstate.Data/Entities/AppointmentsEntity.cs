e36a18452aec01b7c51301eaeeb89743fb2e19b0
hakima amroun <hakimaterki91@gmail.com>
2023-11-14 22:47:56 -0500

    Foreign Keys added

--- RealEstate.Data/Entities/AppointmentsEntity.cs
+++ RealEstate.Data/Entities/AppointmentsEntity.cs
@@ -13,17 +13,22 @@ namespace RealEstate.Data.Entities
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required, ForeignKey(nameof(agent))]
         public int AgentId { get; set; }
+        public AgentsEntity agent { get; set; }= null!;
 
-        [Required]
+        [Required ,ForeignKey(nameof(buyer))]
+        public BuyersEntity  buyer{ get; set; }=null!;
         public int BuyerId{ get; set; }
 
-        [Required]
+        [Required, ForeignKey(nameof(listing))]
         public int ListingId { get; set; }
-        public DateTime DateCreated { get; set; }
+        public Listings listing { get; set; }=null!;
+        public DateTime DateScheduled{ get; set; }
 
         [Required]
         public string FeedBack{ get; set; }= string.Empty;
+
+ 
+        }
     }
-}
