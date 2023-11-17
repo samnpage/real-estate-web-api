b47d7ff0a4955d2f2330cb95147ff4e67a2391aa
Sam Page <samnpage96@gmail.com>
2023-11-17 15:32:26 -0500

    Changed MaxLength for names to the value of 50

--- RealEstate.Data/Entities/AgentEntity.cs
+++ RealEstate.Data/Entities/AgentEntity.cs
@@ -4,10 +4,10 @@ using Microsoft.AspNetCore.Identity;
 namespace RealEstate.Data.Entities;
 public class AgentEntity : IdentityUser<int>
 {
-    [MaxLength(100)]
+    [Required, MaxLength(50)]
     public string FirstName { get; set; } = string.Empty;
 
-    [MaxLength(100)]
+    [Required, MaxLength(50)]
     public string LastName { get; set; } = string.Empty;
 
     [Required]
