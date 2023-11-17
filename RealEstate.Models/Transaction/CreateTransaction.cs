b3cab938964e04fa97dd29dbeb59276a3b8e4b37
Robert Casad <rjcasad@me.com>
2023-11-16 20:22:08 -0500

    Added controller and updatedt transaction model

--- RealEstate.Models/Transaction/CreateTransaction.cs
+++ RealEstate.Models/Transaction/CreateTransaction.cs
@@ -4,5 +4,7 @@ namespace RealEstate.Models.Transaction;
 public class CreateTransaction
 {
     [Required]
+    public int ListingId { get; set; }
+    public int BuyerId { get; set; }
     public int SalePrice { get; set; }
 }
